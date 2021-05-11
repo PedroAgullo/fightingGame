3915a6df39239ed3b48829c6386cf117c0e5d116
Pedro <Pedro.agullo.marco@gmail.com>
2021-05-11 23:59:19 +0200

    Añadidos botones de pelea. Los jugadores se quitan vida y gana 1. Funcion de cambiarestado de boton.

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
+
 let equipo1 = [];
 let equipo2 = [];
-let j1 = [];
-let j2 = [];
-let ronda = 1;
+
+
 
 const cambiaPantalla = (foco) => {
     let arrPantalla = ["pantalla1", "pantalla2", "pantalla3", "pantalla4", "pantalla5", "pantalla6"];
@@ -23,8 +23,8 @@ const escenarioSel = (mapa, escenario) => {
 }
 
 
-let i = 0;
-let x = 0;
+let i = 0; //recorre el array del equipo1
+let x = 0; //recorre el array del equipo2
 const luchadorSel = (luchador) => {
     
     if(equipo2.length < 3){
@@ -33,7 +33,6 @@ const luchadorSel = (luchador) => {
             let seleccionado = "select" + (equipo1.length + 1);
             equipo1.push(listaPersonajes[luchador]);
             document.getElementById(seleccionado).src=equipo1[i].imagen;
-            console.log(i);
             i++;
 
         } else {
@@ -54,11 +53,47 @@ const luchadorSel = (luchador) => {
     }        
 }
 
-const comienzaPartida = () => {
-    let j1 = equipo1[0];
-    let j2 = equipo2[0];
-
-    
+let j1 = [];
+let j2 = [];
+let round = 1;
 
 
+
+
+
+const comienzaPartida = (player) => {          
+    console.log(equipo1[0]);
+    console.log(equipo2[0]);
+    if (player == 1){
+        console.log("Ataca el jugador 1.");
+        equipo1[0].atacar(equipo2[0]);
+    }else{
+        console.log("Ataca el jugador 2.")
+        equipo2[0].atacar(equipo1[0]);
+    }
+
+
+    if (equipo1[0].vida <= 0){
+        console.log("Ha ganado la partida el equipo 1 con el luchador: ", equipo1[0].nombre);
+    }else if(equipo2[0].vida <= 0){
+        console.log("Ha ganado la partida el equipo 2 con el luchador: ", equipo2[0].nombre);
+    }
+
+    console.log("Jugador 1: ", equipo1[0].nombre, " le queda la vida: ", equipo1[0].vida);
+    console.log("Jugador 1: ", equipo2[0].nombre, " le queda la vida: ", equipo2[0].vida);
+    cambiarBoton(player);
+}
+
+
+const cambiarBoton = (player) => {
+    if(player == 1){
+        console.log("Paso por arriba");
+        document.getElementById('boton1').disabled = 'true';
+        document.getElementById('boton2').disabled = 'false';
+
+    }else if (player == 2){
+        console.log("Paso por abajo");
+        document.getElementById('boton1').disabled = 'false';
+        document.getElementById('boton2').disabled = 'true';
+    }
 }
--- js/personajes.js
+++ js/personajes.js
@@ -1,11 +1,12 @@
 
 class Luchador{
-    constructor(nombre,ataque,defensa,imagen){
+    constructor(nombre,ataque,defensa,velocidad,imagen){
         this.vida = 100;
         this.nombre = nombre;
         this.ataque = ataque;
         this.defensa = defensa;
-        this.imagen = imagen;        
+        this.imagen = imagen; 
+        this.velocidad = velocidad;       
     }
     atacar(enemy){
         enemy.vida -= this.ataque - enemy.defensa;
@@ -13,16 +14,16 @@ class Luchador{
 }
 
 //Instancio luchadores
-let p1 = new Luchador("Shrek",30,10,"img/p1.png");
-let p2 = new Luchador("Burro",35,12,"img/p2.png");
-let p3 = new Luchador("Gato",25,20,"img/p3.png");
-let p4 = new Luchador("Fiona",40,8,"img/p4.png");
-let p5 = new Luchador("Frodo",30,10,"img/p5.png");
-let p6 = new Luchador("Slayer",35,12,"img/p6.png");
-let p7 = new Luchador("AmongUs",25,20,"img/p7.png");
-let p8 = new Luchador("Steve",40,8,"img/p8.png");
-let p9 = new Luchador("Dhalsim",30,10,"img/p9.png");
-let p10 = new Luchador("EHonda",35,12,"img/p10.png");
+let p1 = new Luchador("Shrek",30,10,25,"img/p1.png");
+let p2 = new Luchador("Burro",35,12,33, "img/p2.png");
+let p3 = new Luchador("Gato",25,20, 24,"img/p3.png");
+let p4 = new Luchador("Fiona",40,8,33,"img/p4.png");
+let p5 = new Luchador("Frodo",30,10,18,"img/p5.png");
+let p6 = new Luchador("Slayer",35,12,34, "img/p6.png");
+let p7 = new Luchador("AmongUs",25,20,28,"img/p7.png");
+let p8 = new Luchador("Steve",40,8,27,"img/p8.png");
+let p9 = new Luchador("Dhalsim",30,10,26,"img/p9.png");
+let p10 = new Luchador("EHonda",35,12,32,"img/p10.png");
 
 let listaPersonajes = {
     "1" : p1,
